e0e2758d2ee1ce8b8eaacabd6d08d9f954ee22c3
p.kliukin <p.kliukin@gaijin.team>
2021-03-26 13:00:10 +0300

    Change structure of project catalogue to use separate files in templates + add short project description

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# comix-constructor
+# comics-constructor
+Сайт, с помощью которого можно создавать интерактивные истории.
 
 ## Project setup
 ```
@@ -19,6 +20,3 @@ yarn build
 ```
 yarn lint
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/HelloWorld/template.vue
+++ src/components/HelloWorld/template.vue
@@ -33,29 +33,5 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
-
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<script lang="ts" src="./HelloWorld.ts"></script>
+<style lang="scss" src="./hello-world.scss"></style>
